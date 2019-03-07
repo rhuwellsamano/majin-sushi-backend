ddf03e0212418861f9e178836e6c3787862586e7
rhuwellsamano <rhuwell.samano@gmail.com>
2019-03-07 16:44:27 -0500

    added Money column to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
      private
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :money)
     end
 
 end
---
+++ db/migrate/20190307213911_add_money_to_user.rb
@@ -0,0 +1,5 @@
+class AddMoneyToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :money, :integer
+  end
+end
