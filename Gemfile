e482b38fcbb6becb059dd3ed90ffb9ebe8a4b2e4
sabud1208 <43481963+sabud1208@users.noreply.github.com>
2019-05-02 21:56:58 -0400

    seed

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
-
+gem 'rb-readline', '~> 0.5.5'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sushi1 = Sushi.create(name: "Yum Roll", description: "Yummy", image_url: "https://png.pngtree.com/elem
+ent_origin_min_pic/16/10/08/1557f8a2b808dab.jpg", price: 12)
+
+sushi2 = Sushi.create(name: "Dragon Roll", description: "Chewy", image_url: "https://images.megapixl.com/4355/43554953.jpg", price: 15)
+sushi2 = Sushi.create(name: "Hella Good", description: "Sooooo Tasty!!", image_url: "https://res.cloudinary.com/teepublic/image/private/s--cH1BhoH1--/t_Preview/b_rgb:191919,c_limit,f_auto,h_313,q_90,w_313/v1515576664/production/designs/2268991_1", price: 15)
